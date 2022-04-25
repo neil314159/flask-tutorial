9c0c2dc03af50731ad9af9b4f2d7fca1908d9873
neil314159 <neil@neilboland.com>
2022-04-25 17:17:49 +0000

    Update hidden variables

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py")
 
 app = Flask(__name__)
 
@@ -29,9 +30,13 @@ def about_member(member_name):
 
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
